6e66358d65bcb0eeaffc735049fdbbec809b20ba
Atividade3 <donovan@telegram.br>
2024-02-29 16:32:17 -0300

    aa

--- src/br/edu/fatec/controleversao/Main.java
+++ src/br/edu/fatec/controleversao/Main.java
@@ -1,7 +1,11 @@
 package br.edu.fatec.controleversao;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Somar calc = new Somar();
+        System.out.println(calc.soma (20, 10));
     }
 }
--- src/br/edu/fatec/controleversao/Somar.java
+++ src/br/edu/fatec/controleversao/Somar.java
@@ -1,2 +1,8 @@
-package br.edu.fatec.controleversao;public class Somar {
+package br.edu.fatec.controleversao;
+
+public class Somar {
+    public double soma(double num1, double num2) {
+        return num1 + num2;
+    }
+
 }
