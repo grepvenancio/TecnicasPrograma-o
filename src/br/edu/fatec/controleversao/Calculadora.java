151242ac6766f61d8b59cfd06f4cb910f42bc4b5
Atividade3 <donovan@telegram.br>
2024-02-29 17:33:05 -0300

    Calculadora

--- src/br/edu/fatec/controleversao/Calculadora.java
+++ src/br/edu/fatec/controleversao/Calculadora.java
@@ -4,5 +4,14 @@ public class Calculadora {
     public double soma(double num1, double num2) {
         return num1 + num2;
     }
+    public double dividi(double num1, double num2){
+        return num1 / num2;
+    }
+    public double multiplica(double num1, double num2){
+        return num1 * num2;
+    }
+    public double subtrai(double num1, double num2){
+        return num1 - num2;
+    }
 
 }
--- src/br/edu/fatec/controleversao/Main.java
+++ src/br/edu/fatec/controleversao/Main.java
@@ -1,11 +1,40 @@
 package br.edu.fatec.controleversao;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int operação;
+        double num1,num2, resultado;
 
-        Somar calc = new Somar();
-        System.out.println(calc.soma (20, 10));
+        Calculadora calc = new Calculadora();
+        Scanner ler = new Scanner(System.in);
+        while(true) {
+            System.out.println("Digite a operação: \n 1- soma \n 2- divisão \n 3- multiplica \n 4- subtração \n 5- sair");
+            operação = ler.nextInt();
+            if (operação == 5)
+                break;
+            System.out.println("Digite o primeiro valor: ");
+            num1 = ler.nextDouble();
+            System.out.println("Digite o segundo valor: ");
+            num2 = ler.nextDouble();
+
+
+            switch(operação) {
+                case 1:
+                    resultado = calc.soma(num1, num2);
+                    break;
+                case 2:
+                    resultado = calc.dividi(num1, num2);
+                    break;
+                case 3:
+                    resultado = calc.multiplica(num1, num2);
+                    break;
+                case 4:
+                    resultado = calc.subtrai(num1, num2);
+                    break;
+            }
+        }
     }
 }
